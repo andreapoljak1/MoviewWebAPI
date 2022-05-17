9e45880b1092fa21b00fafadf1f093e7006062c3
Andrea Poljak <andreapoljak1>
2022-05-17 18:11:16 +0200

    Add project files.

---
+++ Movies.API/Controllers/MoviesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Data.Interfaces;
+using Movies.Data.Models;
+using Movies.Data.Repositories;
+
+namespace Movies.API.Controllers
+{
+    //RUTA: localhost:8000/api/Movies
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        //konstruktor
+        public MoviesController (IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        //GET: api/Movies
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> GetMovies()
+        {
+            //moramo napraviti inection klase repozitorija
+            try
+            {
+                return Ok(_movieRepository.GetAll());
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            
+        }
+
+        [Route("api/Movies")]
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult FindMovies(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest("Nije moguće prikazati rezultate, dogodila se greška!");
+            }
+            var movie = _movieRepository.GetMovieById(id);
+            if (movie == null)
+            {
+
+                return NotFound("Rezultat nije pronađen");
+            }
+            else
+            {
+                return Ok(movie);
+            }
+
+        }
+
+    }
+}
---
+++ Movies.Data/Repositories/MovieRepository.cs
@@ -0,0 +1,50 @@
+﻿using Movies.Data.Interfaces;
+using Movies.Data.Models;
+
+namespace Movies.Data.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        //injektiramo clasu
+        private readonly algebramssqlhost_moviesContext _context;
+
+        //konstruktor
+        public MovieRepository(algebramssqlhost_moviesContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Movie> GetAll()
+        {
+            return _context.Movies.ToList();
+        }
+        public Movie GetMovieById(int id)
+        {
+            return _context.Movies.FirstOrDefault(s=>s.Id==id);
+        }
+        public Movie InsertMovie(Movie new_movie)
+        {
+            return null;
+        }
+        public Movie UpdateMovie(Movie new_movie)
+        {
+            return null;
+        }
+        public Movie DeleteMovie(int id)
+        {
+            return null;
+        }
+
+        public IEnumerable<Movie> QueryStringFilter(string s, string orderby,int per_page)
+        {
+            return null;
+        }
+
+
+
+
+
+
+
+
+    }
+}
